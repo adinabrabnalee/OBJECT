82cb3caed9224dc2fd8d130ca799886c11d919cf
adinabrabnalee <143054157+adinabrabnalee@users.noreply.github.com>
2023-11-25 21:01:58 +0800

    initial change

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native';
+import { Button } from '@rneui/themed';
+import { users } from './info';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 export default function App() {
+  const showFullname = (users) =>{
+    alert(users)
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+      <ScrollView>
+      <Entypo/>
+      <FlatList
+        data = {users}
+        keyExtractor = {(index) => index.toString()}
+        renderItem = {({item}) => (
+        <View style = {styles.user}>
+            <View style = {styles.nnCon}>
+              <Text style = {styles.name}>{item.nickname}</Text>
+            </View>
+            <View style = {styles.buttonCont}>
+              <Button title = "View"
+              onPress = {()=>showFullname(`${'Name: '}${item.name.firstname}${' '}${item.name.lastname}${'\n'}${'Course: '}${item.course}${'\n'}${'Year: '}${item.year}`)}
+              buttonStylestyle = {styles.buttonVw}/>
+            </View>
+        </View>     
+        )}
+      />
+      </ScrollView>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems:'center',
+    marginTop:10,
+    padding:10
   },
+  user: {
+    marginBottom: 10,
+    width:300,
+    flexDirection: 'row',
+  },
+  name: {
+    fontSize: 28,
+    marginTop: 10,
+    fontFamily: 'Times New Roman',
+    fontWeight:'normal',
+  },
+  
+  nnCon:{
+    padding:10,
+    width:200,
+  },
+  buttonCon:{
+    marginTop:10,
+    width:80,
+    flexDirection:'row',
+  },
+  buttonVw:{
+    width:65,
+  }  
 });
---
+++ info.js
@@ -0,0 +1,48 @@
+export const users = [
+    {
+      nickname:'nal',
+      name: {
+        firstname:'ANALIE',
+        lastname:'BARBANIDA',
+      },
+      course:'BSIT',
+      year:'3',
+      
+    },
+    {
+      nickname:'BHE',
+      name: {
+        firstname:'PHOEVE',
+        lastname:'ADINABRAB',
+      },
+      course:'BS CRIM',
+      year:'1',
+    },
+    {
+      nickname:'JACKY',
+      name: {
+        firstname:'JAQUELINE',
+        lastname:'VILLOREJO',
+      },
+      course:'BSIT',
+      year:'3',
+    },
+    {
+      nickname:'SIE',
+      name: {
+        firstname:'ELSIE',
+        lastname:'ASIDELLA',
+      },
+      course:'BSFM',
+      year:'4',
+    },
+    {
+      nickname:'LONG',
+      name: {
+        firstname:'CLYDE',
+        lastname:'ESPIGADERA',
+      },
+      course:'BSIT',
+      year:'1',
+    },
+  ];
